f810172d0e7cb6bede0be17d2cdead7776a78ad6
Randi <randi.mendel@yahoo.com>
2015-12-06 12:55:29 -0800

    update files

--- index.js
+++ index.js
@@ -2,19 +2,23 @@
 //require express, http server, and socket.io
 var express = require('express');
 var http = require('http');
-var socket = require('socket.io');
+
 
 //create variable called "app", that relies on express
 var app = express();
 //create a variable called "server" on the app to create and run the http server
-var server = http.Server(app);
+var http = http.Server(app);
 //create a variable called "io" that runs socket.io on the http server
-var io = socket()(http);
+var io = require('socket.io')(http);
 
 //get the directory from "app" and send it the file '/index.html' 
 //index.html is an html file that we have created for you to make the chat button and input on the page
-app.get('/', function(req, res){
-        res.sendFile(__dirname + '/index.html');
+
+app.get('/static', function(req, res){
+    app.use(express.static(__dirname + '/static'));
+ //   res.sendFile(__dirname + 'static/index.html');
+    //app.use(express.static('static'));
+
 });
 
 //on the "io" variable, when there is a connection, execute a function that emits the chat message when one is sent
