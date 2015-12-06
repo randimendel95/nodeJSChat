318a7eeff91c38d799e88d1c7fef06d3203729dc
Randi <randi.mendel@yahoo.com>
2015-12-06 12:02:57 -0800

    add comments to index.js

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
 
+//require express, http server, and socket.io
+var express = require('express');
+var http = require('http');
+var socket = require('socket.io');
+
+//create variable called "app", that relies on express
+var app = express();
+//create a variable called "server" on the app to create and run the http server
+var server = http.Server(app);
+//create a variable called "io" that runs socket.io on the http server
+var io = socket()(http);
+
+//get the directory from "app" and send it the file '/index.html' 
+//index.html is an html file that we have created for you to make the chat button and input on the page
 app.get('/', function(req, res){
         res.sendFile(__dirname + '/index.html');
 });
 
+//on the "io" variable, when there is a connection, execute a function that emits the chat message when one is sent
 io.on('connection', function(socket){
         socket.on('chat message', function(msg){
                 io.emit('chat message', msg);
